b9f4c488143f5a0f292f43705edca7d23df01633
杨尚山 <cn.yangss@gmail.com>
2024-07-19 18:46:50 +0800

    去掉之前src

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,68 @@
-pub mod init;
 
-#[cfg(feature = "pretrain")]
-pub mod pretrain;
+pub fn setup_tracing(log_level: &str, logfile: Option<&PathBuf>, display_target: Option<bool>) {
+    static START: Once = Once::new();
+    START.call_once(|| {
+        let level = Level::from_str(log_level).expect("Invalid log level");
+        // tracing_subscriber::fmt::init();
 
-#[cfg(feature = "pretrain")]
-pub mod prelude {
-    pub use crate::init::args;
-    pub use crate::init::init_log;
-    pub use crate::init::CommandArgs;
+        let l = tracing_subscriber::fmt::layer::<Registry>()
+            .with_thread_names(true)
+            .with_target(display_target.unwrap_or(false));
 
-    pub use crate::pretrain::chunks_timeout_of_train_files;
-    pub use crate::pretrain::get_imported_lines;
-    pub use crate::pretrain::pretrain_do;
-    pub use crate::pretrain::pretrain_sink;
-    pub use crate::pretrain::records_of_train_files;
-    pub use crate::pretrain::spawn_pretrain_task;
+        if let Some(logfile) = logfile {
+            let file_appender = tracing_appender::rolling::hourly(
+                logfile.parent().unwrap(),
+                logfile.file_name().unwrap(),
+            );
+            let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+            let f = l
+                .with_writer(non_blocking)
+                .with_filter(EnvFilter::from_default_env().or(LevelFilter::from_level(level)));
+            let layered = tracing_subscriber::registry().with(f);
+            tracing::subscriber::set_global_default(layered)
+                .expect("setting default subscriber failed");
+        } else {
+            let f = l.with_filter(EnvFilter::from_default_env().or(LevelFilter::from_level(level)));
+            let layered = tracing_subscriber::registry().with(f);
+            tracing::subscriber::set_global_default(layered)
+                .expect("setting default subscriber failed");
+        }
+
+        info!(
+            "OK. setup tracing, log level: {:?}, RUST_LOG={}",
+            &level,
+            env::var("RUST_LOG").unwrap()
+        );
+    });
+}
+
+lazy_static! {
+    static ref TOKIO_RUNTIME: OnceLock<Runtime> = OnceLock::new();
+}
+
+pub fn runtime() -> &'static Runtime {
+    init();
+    info!("Commond args: {:?}", args());
+    TOKIO_RUNTIME.get_or_init(|| {
+        runtime::Builder::new_multi_thread()
+            .worker_threads(4)
+            .max_blocking_threads(8)
+            .thread_name_fn(|| {
+                static ATOMIC_ID: AtomicUsize = AtomicUsize::new(0);
+                let id = ATOMIC_ID.fetch_add(1, Ordering::SeqCst);
+                format!("tokio-{}", id)
+            })
+            .disable_lifo_slot()
+            .enable_all()
+            .enable_metrics_poll_count_histogram()
+            .metrics_poll_count_histogram_scale(HistogramScale::Log)
+            .metrics_poll_count_histogram_buckets(15)
+            .metrics_poll_count_histogram_resolution(Duration::from_micros(100))
+            .build()
+            .unwrap()
+    })
+}
+
+pub fn block_on<F: Future>(future: F) -> F::Output {
+    runtime().block_on(future)
 }
